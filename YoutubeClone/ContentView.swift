eef180bfe8e48cc4d4f65f79354f9db5c7b0e911
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-11-02 22:22:16 +0700

    Initial Commit

---
+++ YoutubeClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YoutubeClone
+//
+//  Created by Ricky Primayuda Putra on 02/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YoutubeClone/YoutubeCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeCloneApp.swift
+//  YoutubeClone
+//
+//  Created by Ricky Primayuda Putra on 02/11/24.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
